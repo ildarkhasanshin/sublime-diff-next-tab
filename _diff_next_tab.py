dc853e81c8ca474f5e5b4534c0d56e51657c5f4f
ildar r. khasanshin <ildar.khasanshin@gmail.com>
2017-01-09 11:41:35 +0500

    spaces, intervals

--- _diff_next_tab.py
+++ _diff_next_tab.py
@@ -1,21 +1,20 @@
 import sublime, sublime_plugin
 import subprocess
+
 class DiffCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		cur = num = 0
-		# Получим массив всех view
-		views = sublime.active_window().views()
-		# Пройдемся по всем view в поисках активного, сравнивая по id
-		for view in views:
-			if view.id() == sublime.active_window().active_view().id():
-				# Если id совпало, то запомним номер активного view
-				cur = num
-			num += 1
-		# Проверим не является ли активный view последним
-		if num-1 != cur:
-			# Запустим программу meld с параметрами
-			subprocess.Popen([
-				'meld',
-				views[cur].file_name(),
-				views[cur+1].file_name()
-			])
+
+  def run(self, edit):
+    cur = num = 0
+    views = sublime.active_window().views()
+
+    for view in views:
+      if view.id() == sublime.active_window().active_view().id():
+        cur = num
+      num += 1
+
+    if num-1 != cur:
+      subprocess.Popen([
+        'meld',
+        views[cur].file_name(),
+        views[cur+1].file_name()
+      ])
